e3d3d13e07e7c6c06f3ddb087f8b47d2b21b19dd
Sujata Chaudhary <076bct085.sujata@pcampus.edu.np>
2023-02-27 08:13:08 +0545

    player made

--- bubbletrouble/lib/HomePage.dart
+++ bubbletrouble/lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:bubbletrouble/button.dart';
+import 'package:bubbletrouble/player.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,38 +10,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+      double playerX = 0;
+  void moveRight() {
+    setState(() {
+      playerX += 0.1;
+    });
+  }
+
+  void moveUp() {}
+  void moveLeft() {
+    setState(() {
+      playerX -= 0.1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Expanded(
-            flex: 3,
-            child: Container(
-              color: Color.fromARGB(190, 134, 255, 229),
-              child: Container(child: Stack(
+          flex: 3,
+          child: Container(
+            color: Color.fromARGB(190, 134, 255, 229),
+            child: Center(
+              child: Stack(
                 children: [
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.yellow,
-                  )
+                  MyPlayer(playerX)
                 ],
-              ),),),)
-        ,Expanded(
+              ),
+            ),
+          ),
+        ),
+        Expanded(
           child: Container(
             color: Colors.blueGrey,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-              MyButton(icon: Icons.arrow_forward,),
-              MyButton(icon: Icons.arrow_upward,),
-              MyButton(icon: Icons.arrow_downward,),
-
-            ]),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MyButton(icon: Icons.arrow_back, function: moveLeft),
+                  MyButton(icon: Icons.arrow_upward, function: moveUp),
+                  MyButton(icon: Icons.arrow_forward, function: moveRight),
+                ]),
           ),
-          
         )
       ],
-    ); 
+    );
   }
 }
--- bubbletrouble/lib/button.dart
+++ bubbletrouble/lib/button.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  MyButton({required this.icon,super.key});
+  MyButton({required this.icon, super.key, this.function});
   IconData icon;
+  final function;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        width: 70,
-        height: 70,
-        color: Color.fromARGB(240, 255, 255, 255),
-        child: Icon(icon),
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          width: 70,
+          height: 70,
+          color: Color.fromARGB(240, 255, 255, 255),
+          child: Center(child: Icon(icon),)
+        ),
       ),
     );
   }
